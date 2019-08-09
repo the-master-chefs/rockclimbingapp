f6757620b88c1a5b3612d66421b3ca3e4ad546df
Krayt0n3 <hendersonkevin64@gmail.com>
2019-08-08 21:16:03 -0500

    adding build for giggles

---
+++ client/build/precache-manifest.737983d27e234680cd534723a2e17a6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501d2153f88d2504196ff7e1fd0e846b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a7897f5bb5ec5700684",
+    "url": "/static/css/main.691e2ecf.chunk.css"
+  },
+  {
+    "revision": "13f58561fa6a5b38a61e",
+    "url": "/static/js/2.11c0defe.chunk.js"
+  },
+  {
+    "revision": "8a7897f5bb5ec5700684",
+    "url": "/static/js/main.4e787e3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c2d3c01206754183d6febd54b1c25ea",
+    "url": "/static/media/giphy.2c2d3c01.gif"
+  }
+]);
